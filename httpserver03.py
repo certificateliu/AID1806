90af035b9f88ff32c8ecc8d55727dda8d2e1e391
Jame <liujingwei@tedu.com>
2018-08-29 11:10:07 +0800

    add a file:httpserver03.py

---
+++ httpserver03.py
@@ -0,0 +1,54 @@
+# httpserver03.py
+# HTTPServer + Frame 版本
+
+# httpserver 功能：
+#     １、接收http请求　(socket通信)
+#     ２、解析http请求　
+#         请求类型　　GET, POST
+#         请求内容　　
+#     ３、将具体的请求整理后给　web Frame 
+#     ４、接收后端应用返回的数据内容
+#     ５、将数据组织为http response的格式发给客户端
+
+# Frame 功能：
+#     框架：即开发模板，根据框架规则填写内容即可快速完成开发工作
+
+#     １、接收　httpserver 部分发送的请求
+#     ２、根据请求进行逻辑处理或数据整合
+#     ３、将处理结果回发给　httpserver 部分
+#         １）可以处理，则结果和数据都需要给　httpserver
+#         ２）可能无法处理，给　httpserver　原因
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
